1b777498773fc9e33ce662a90b95c5b8f541d877
Rafi Ullah <krafi0500@gmail.com>
2026-07-03 21:38:23 +0000

    Variables

---
+++ new/variables.js
@@ -0,0 +1,10 @@
+
+
+const accId = 12345
+let accEmail ="Rafo@gmail.com"
+var accPassword ="1234"
+accCity = "Swat"
+
+let accState
+
+console.table([accId ,accEmail ,accPassword , accCity , accState]);
